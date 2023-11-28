90d2196c35230a1a984c505f6a81e2ce701f3c9c
landonjett <landonjett@gmail.com>
2023-11-27 21:14:30 -0600

    added multiple functions, global variables, and event listeners. starting to get a responsive webpage

--- assets/script.js
+++ assets/script.js
@@ -47,3 +47,52 @@ const questions = [
                             "declare function myFunction()"],
                             correctAnswer: "function myFunction()"},
 ]
+
+// Global variables
+let currentQuestionIndex = 0;
+let timer;
+let timeRemaining = 30; // Initial time in seconds
+const timeDisplay = document.getElementById("time");
+const questionContainer = document.getElementById("question-container");
+const answerButtons = document.getElementById("answer-buttons");
+const startButton = document.getElementById("start-button");
+const resultDisplay = document.getElementById("result");
+const scoreForm = document.getElementById("score-form");
+const initialsInput = document.getElementById("initials");
+const scoreList = document.getElementById("score-list");
+const highScoresContainer = document.getElementById("high-scores");
+const clearScoresButton = document.getElementById("clear-scores");
+
+// Event listeners
+startButton.addEventListener("click", startQuiz);
+answerButtons.addEventListener("click", selectAnswer);
+scoreForm.addEventListener("submit", saveScore);
+clearScoresButton.addEventListener("click", clearScores);
+
+// Function to start the quiz
+function startQuiz() {
+  startButton.classList.add("hide");
+  showQuestion(questions[currentQuestionIndex]);
+  timer = setInterval(updateTimer, 1000);
+}
+// Function to display a question
+function showQuestion(question) {
+  questionContainer.textContent = question.question;
+  answerButtons.innerHTML = "";
+  question.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.textContent = answer;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+  });
+}
+
+// Function to update the timer
+function updateTimer() {
+  timeDisplay.textContent = timeRemaining;
+  timeRemaining--;
+
+  if (timeRemaining < 0) {
+    endQuiz();
+  }
+}
