37859a19d55dec50e8e39e05ce6a21a3658e9cc3
landonjett <landonjett@gmail.com>
2023-11-27 21:11:19 -0600

    added questions for the quiz along with starter java script

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+// Quiz questions and answers
+const questions = [
+  {
+    question: "What is JavaScript?",
+    answers: [
+      "A programming language",
+      "A type of coffee",
+      "A dinosaur",
+      "A fruit"
+    ],
+    correctAnswer: "A programming language"
+  },
+
+  {
+  question: "What keyword is used to declare a variable in JavaScript?",
+  answers: [
+      "var",
+      "int",
+      "string",
+      "let"],
+      correctAnswer: "var"},
+
+      {
+        question: "Which statement is used to execute a block of code repeatedly?",
+        answers: [
+            "if",
+            "for",
+            "switch",
+            "do-while"],
+            correctAnswer: "for"},
+
+            {
+                question: "How do you comment a single line in JavaScript?",
+                answers: [
+                    "/* comment */",
+                    "// comment",
+                    "<!-- comment -->",
+                    "/comment/"],
+                    correctAnswer: "// comment"},
+    
+                    {
+                        question: "How do you declare a function in JavaScript?",
+                        answers: [
+                            "create function myFunction()",
+                            "function: myFunction()",
+                            "function myFunction()",
+                            "declare function myFunction()"],
+                            correctAnswer: "function myFunction()"},
+]
